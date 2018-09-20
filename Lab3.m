3fca20c29ba7e9ae964245032e9856c90322a607
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-09-19 19:57:48 -0500

    updating

--- Lab3.m
+++ Lab3.m
@@ -1,9 +1,14 @@
-clc
-clear
+% Alberto Pizano
+% ECE301
+% Lab 3 - Generation of Some Basic CT Signals
+% 9/17/2018
+
+clc;
+clear;
 
 t = -1.0:0.01:3.0;
 
-%part 1.
+%part 1 ------------------------------------------------------------------
 for i=1:length(t)
     if t(i)>= 0
         x(i) = 1;
@@ -19,8 +24,9 @@ xlabel('t (seconds)')
 ylabel('u(t)')
 ylim([-3 3])
 grid on
+%--------------------------------------------------------------------------
 
-%part 2 w/ delay of 0.1 seconds part A)
+%part 2 w/ delay of 0.1 seconds part A)------------------------------------
 t2 = -1.0:0.01:3.0;
 for i=1:length(t2)
     if t2(i)>= 0.1
@@ -37,18 +43,17 @@ xlabel('t (seconds)')
 ylabel('y(t)')
 ylim([-3 3])
 grid on
+%-------------------------------------------------------------------------
 
-%part 2 part B)
+%part 2 part B)-----------------------------------------------------------
 dt = 0.01;
 t3 = -1.0:dt:3.0;
 f = 1;
 omega = 2*pi*f;
-
 d = 0.4;
 
 x = 1.5*cos(omega*t3-(pi/4));
 y = 1.5*cos(omega*(t3-d)-(pi/4));
-
 
 %{
 for k=1:length(t3)
@@ -76,8 +81,9 @@ xlabel('t (seconds)')
 ylabel('y(t)')
 ylim([-3 4])
 grid on
+%--------------------------------------------------------------------------
 
-%part 4 table 1
+%part 4 table 1------------------------------------------------------------
 x_of_t1 = 1.5*cos(omega*-0.9-(pi/4))
 x_of_t2 = 1.5*cos(omega*0-(pi/4))
 x_of_t3 = 1.5*cos(omega*0.4-(pi/4))
@@ -99,10 +105,10 @@ y_of_t7 = 1.5*cos(omega*(3.2-d)-(pi/4))
 
 y_of_t8 = 1.5*cos(omega*(3.3-d)-(pi/4))
 y_of_t9 = 1.5*cos(omega*(3.8-d)-(pi/4))
+%--------------------------------------------------------------------------
 
-%part 5 rectpulse
+%part 5 rectpulse----------------------------------------------------------
 
-%{
 t=(-2:.01:2);
 x=rectpuls(t,3.99);
 figure ()
@@ -112,7 +118,7 @@ xlabel("time ")
 title("Pulse Function ")
 axis([-3 3 -3 5])
 grid on 
-%}
+
 
 %{
 dt = 0.01 
@@ -137,8 +143,7 @@ xlim([-5 5])
 ylim([0 5])
 %}
 
-
-%part 6 pulse a second way
+%part 6 pulse a second way ------------------------------------------------
 t4 = -5:0.01:5;
 f2 = 3.5*heaviside(t4+2)-3.5*heaviside(t4-2);
 figure;
@@ -148,12 +153,40 @@ xlabel('t (seconds)')
 ylabel('p(t) amplitude')
 ylim([-3 5])
 grid on
+%--------------------------------------------------------------------------
 
-% part 7 g(t)
+%part 7 sketch of g(t) ----------------------------------------------------
 t5 = 0:0.01:9;
 f3 = 3*(t5+2).*heaviside(t5+2)-6*(t5).*heaviside(t5)+3*(t5-2).*heaviside(t5-2);
+figure;
 plot(t5, f3)
 title('Sketch of g(t)')
 xlabel('t (seconds)')
 ylabel('g(t)')
+grid on
+%--------------------------------------------------------------------------
+
+%part 8/9------------------------------------------------------------------
+t6=[-2:.01:10];
+
+for i=0:9
+    if i<=2
+    x2(i+1)=i;
+    end
+    
+    if i>2&i<=6
+    x2(i+1)=2;
+    end
+    
+    if i>6&i<=9   
+    x2(i+1)=2/3*(9-i);
+    end
+end 
+
+figure;
+plot([0:9],x2);
+axis([-3 ,10 ,-2 ,4]);
+ylabel("Ramp(t)")
+xlabel("time ")
+title("Ramp Function ")
 grid on
