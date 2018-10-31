e26bc28faddf2bf919e3152b8419c0c65085628f
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-10-30 22:29:57 -0500

    fourier series on matlab

--- lab6.m
+++ lab6.m
@@ -1,6 +1,49 @@
-clc
-clear
+%Alberto Pizano
+%Lab6 ECE301
 
-load Lab6SimData2
+%part1
+figure;
+plot(newsim9.time, newsim9.Data) 
+title('step')
+xlabel('time')
+ylabel('Data')
 
-plot(header100, data100) 
+%part2
+omega = logspace(-1,3);
+
+angle = -atand(omega/10); %inverse tangent in degrees
+H = 10./abs(10+j*omega);
+
+figure;
+subplot (2,1,1)
+plot(H)
+%%xticks(0.1:0.05:1000);
+title('Magnitude')
+xlabel('Phase')
+ylabel('H(jw)')
+
+subplot(2,1,2)
+plot(angle) 
+title('Phase angle')
+xlabel('Phase')
+ylabel('Theta')
+
+%part 3
+figure;
+subplot(2,1,1)
+a=[1 10];
+b=10;
+%%w=logspace(-1,3);
+h=freqs(b,a,omega);
+
+loglog(abs(h));
+title('Magnitude')
+xlabel('Phase')
+ylabel('H(jw)')
+grid on
+subplot(2,1,2)
+semilogx(angle);
+title('Phase angle')
+xlabel('Phase')
+ylabel('Theta')
+grid on
