d28e7ef20384bf737116a79c7ca4b3f31982338c
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-11-28 01:34:29 -0600

    updated lab

--- Lab9.m
+++ Lab9.m
@@ -14,7 +14,7 @@ e = exp(1);
 k = -10:1:10;  
 Ck = -2*A./(pi*(4*k.^2-1));
     
-
+figure;
 subplot(211); % 
 stem(k*f0,abs(Ck)); %must be k*f0
 title('|C_k|');
@@ -70,9 +70,87 @@ end
 n = -20:1:20;
 Ck_2_2 =  A./(n*pi).*sin((2*pi*n*a)./T) 
 
-
+figure;
 stem(n*f0_2, abs(Ck_2_2));
+hold on;
+title('|C_n| with T=10ms');
+xlabel('n*f0');
+%xlim([-1200 1200]);
+ylabel('|C_n|');
+
+% PART 3
+a = .002
+A = 1;
+T = .05;
+f0_3 = 1/T;
+w0_3 = 2*pi*f0_3;
+
+k = -100:1:100;
+Ck_3 = ((A)./(k*pi)).*sin(k*w0_3*a);
+
+figure;
+stem(k*f0_3, abs(Ck_3));
+hold on;
+xlabel('k*f0');
+ylabel('|Ck|');
+title('|C_k| with T=50ms');
 
 
+% PART 4
+a = .002;
+A = 1;
+T_2 = 0.05;
+f0_4 = 1/T_2;
+w0_4 = 2*pi*f0_4;
+
+k = -200:1:200;
+Ck_4 = ((A)./(k*pi)).*sin(k*w0_4*a);
+
+figure;
+stem(k*f0_4, abs(Ck_4));
+xlabel('k*f0');
+ylabel('|C_k|');
+title('|C_k| with T=100ms');
+hold on;
+
+% PART 5
 
 
+% PART 6
+a = 0.002;
+df = .5;
+fr = -2000:df:2000;
+Xf = (a*(sin(pi*fr*a))./(pi*fr*a))
+
+figure;
+plot(fr,Xf)
+grid on;
+xlabel('f')
+ylabel('X(f)')
+title('Periodic Signal of Fig.1')
+
+% PART 7
+a = .004;
+df = .5;
+fr = -2000:df:2000;
+Xf_2 = (a*sin(pi*fr*a))./(pi*fr*a);
+
+figure;
+plot(fr, Xf_2)
+grid on;
+xlabel('f')
+ylabel('X(f)')
+title('Fig.2 8ms Rectangular Pulse Width')
+
+% PART 8 
+a = 0.01;
+df = 0.5;
+fr = -2000:df:2000;
+Xf_3 = (a*sin(pi*fr*a))./(pi*fr*a);
+
+figure;
+plot(fr,Xf_3)
+grid on;
+xlabel('f')
+ylabel('X(f)')
+title('Fig.2 20ms Rectangular Pulse Width')
